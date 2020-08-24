844d2d6652bfc947ed02a4080ab01c15e1acad28
jyoti-thasal <jthasal@gmail.com>
2020-08-24 17:11:23 +0530

    seprate menus for shopping list and recipe

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+recipes:Recipe[]=[
+  new Recipe('a Test Recipe','This is simply a test','https://www.julieseatsandtreats.com/wp-content/uploads/2017/05/Baked-Barbecue-Chicken-Drumsticks-6.jpg'),
+  new Recipe('a Test Recipe','This is simply a test','https://www.julieseatsandtreats.com/wp-content/uploads/2017/05/Baked-Barbecue-Chicken-Drumsticks-6.jpg')
 
+];
   constructor() { }
 
   ngOnInit(): void {
